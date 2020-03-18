48fda8719593314af9fc5d17bbdfa009709e5ded
Alex Sanders <a@snd.rs>
2020-03-18 15:22:00 +0000

    use ok yarn when running in the host's node

--- src/run.ts
+++ src/run.ts
@@ -1,3 +1,4 @@
+import chalk from 'chalk'
 import execa from 'execa'
 import fs from 'fs'
 import nvexeca from 'nvexeca'
@@ -7,40 +8,48 @@ import satisfies from 'semver/functions/satisfies'
 import { getNodeRange } from './get-node-range'
 import { info } from './log'
 
+const tick = chalk.green('✓')
+
+const workingEnv = (node: string, pm: string) =>
+	`${tick} Using ${pm} and Node v${coerce(node)}`
+
 export const run = async (tasks: string[] = []) => {
 	const nodeRange = getNodeRange()
 
 	const useCurrentNode = satisfies(process.version, nodeRange)
 
 	const yarnLockPath = path.resolve(process.cwd(), 'yarn.lock')
-	const packageManager = fs.existsSync(yarnLockPath) ? 'Yarn' : 'npm'
+	const packageManager = fs.existsSync(yarnLockPath) ? 'yarn' : 'npm'
 
-	const xEca = (args: string[]) =>
+	const _execa = (args: string[]) =>
 		useCurrentNode
 			? execa(packageManager, args, {
 					stdio: 'inherit',
+					localDir: __dirname,
+					preferLocal: true,
 			  }).then(result => ({
 					childProcess: result,
 					version: process.version,
 			  }))
 			: nvexeca(nodeRange, packageManager, args, {
 					stdio: 'inherit',
+					localDir: __dirname,
 					progress: true,
 			  })
 
 	try {
-		if (useCurrentNode)
-			info(`Using Node v${coerce(process.version)} and ${packageManager}`)
+		if (useCurrentNode) info(workingEnv(process.version, packageManager))
 
-		const { childProcess: installDeps, version } = await xEca([
+		const { childProcess: installDeps, version } = await _execa([
 			'install',
 			'--silent',
 		])
 
-		if (!useCurrentNode)
-			info(`Using Node v${coerce(version)} and ${packageManager}`)
+		if (!useCurrentNode) info(workingEnv(version, packageManager))
 
 		await installDeps
+
+		info(`${tick} All dependencies up-to-date`)
 	} catch (e) {
 		console.error(e)
 		process.exit(1)
@@ -49,5 +58,5 @@ export const run = async (tasks: string[] = []) => {
 	// spacer
 	console.log('')
 
-	return xEca(['run', '--silent', ...tasks])
+	return _execa(['run', '--silent', ...tasks])
 }
